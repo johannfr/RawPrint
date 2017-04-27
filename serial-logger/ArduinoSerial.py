d505e1f724f1108efd26cbf47d2d51d32c00646e
Johann Fridriksson <jofr@foss.dk>
2017-04-27 10:32:41 +0200

    Added uint8_t and uint32_t and modified the existing int to uint16_t

--- serial-logger/ArduinoSerial.py
+++ serial-logger/ArduinoSerial.py
@@ -3,23 +3,46 @@ import struct
 
 class ArduinoSerial(serial.Serial):
 
-    def read_int(self):
-        raw_data = self.read(2)
-        int_value = int(struct.unpack("<h", raw_data)[0])
+    def read_uint8_t(self):
+        raw_data = self.read(1)
+        try:
+            int_value = int(struct.unpack("<B", raw_data)[0])
+        except struct.error:
+            return None
         return int_value
+
+    def read_uint16_t(self):
+        raw_data = self.read(2)
+        try:
+            int_value = int(struct.unpack("<H", raw_data)[0])
+        except struct.error:
+            return None
+        return int_value
+
+    def read_uint32_t(self):
+        raw_data = self.read(2)
+        try:
+            int_value = int(struct.unpack("<I", raw_data)[0])
+        except struct.error:
+            return None
+        return int_value
+
 
     def read_double(self):
         raw_data = self.read(4)
-        double_value = float(struct.unpack("<f", raw_data)[0])
+        try:
+            double_value = float(struct.unpack("<f", raw_data)[0])
+        except struct.error:
+            return None
         return double_value
 
 
 if __name__ == "__main__":
-    s = ArduinoSerial("/dev/ttyUSB0", baudrate=115200, timeout=1)
+    s = ArduinoSerial("/dev/ttyUSB0", baudrate=9600, timeout=1)
 
     try:
         print s.read_double()
-        print s.read_int()
+        print s.read_uint16_t()
     except KeyboardInterrupt:
         pass
     s.flush()
--- serial-logger/Logger.py
+++ serial-logger/Logger.py
@@ -4,6 +4,7 @@
 import sys
 import os
 from ArduinoSerial import ArduinoSerial
+import time
 
 if len(sys.argv) < 4:
     sys.stderr.write("Usage: %s <serial-port> <format-string> <log-file>\n"%sys.argv[0])
@@ -12,8 +13,9 @@ if len(sys.argv) < 4:
 serial_port = sys.argv[1]
 format_string = sys.argv[2]
 log_filename = sys.argv[3]
+start_time = int(time.time())
 
-s = ArduinoSerial(serial_port, baudrate=115200, timeout=1)
+s = ArduinoSerial(serial_port, baudrate=9600, timeout=50)
 
 while True:
     line = s.readline()
@@ -22,14 +24,28 @@ while True:
 
 log_file = open(log_filename, "w")
 
+def gettimestamp():
+    global start_time
+    return int(time.time())-start_time
+
 token_types = {
-    "d" : s.read_int,
-    "f" : s.read_double
+    "s" : s.read_uint8_t,
+    "i" : s.read_uint16_t,
+    "d" : s.read_uint16_t,
+    "l" : s.read_uint32_t,
+    "f" : s.read_double,
+    "t" : gettimestamp
 }
 
 def tee(file_handle, output):
     sys.stdout.write(output)
     file_handle.write(output)
+    file_handle.flush()
+
+
+tee(log_file, "Start-time: %s"%time.strftime("%Y-%m-%d %H:%M:%S"))
+
+
 
 try:
     token_match = False
